d617866f1eb781eecb66f6f777b2f0468b10ed5e
ZixiaoWang <zwan0094@student.monash.edu>
2022-04-12 17:56:45 +1000

    v2.0 update

---
+++ FIT5120Project.2.0/Models/Project.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace FIT5120Project.Models
+{
+    public class Project
+    {
+        public int projectId { get; set; }
+        public bool complete { get; set; }
+        public System.DateTime dueDate { get; set; }
+        public int vote { get; set; }
+        public string projectImagePath { get; set; }
+
+        public virtual ICollection<ProjectMembership> projectMemberships { get; set; }
+    }
+}
---
+++ FIT5120Project.2.0/Models/ProjectMembership.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FIT5120Project.Models
+{
+    public class ProjectMembership
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+
+        public int projectId { get; set; }
+        public virtual Project Project { get; set; }
+    }
+}
